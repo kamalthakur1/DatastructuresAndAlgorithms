afe062e688ea1f46c6f6fee66a2b71625e7bb38f
kamalthakur1 <kamalthakur2195@gmail.com>
2018-05-02 00:14:23 +0530

    added program7

---
+++ program7.cpp
@@ -0,0 +1,11 @@
+//common undersatnding about pointers
+#include<iostream>
+using namespace std;
+int main(){
+    int *p,*j,a,b;
+    j = p;
+    cout<<j<<endl;
+    j = &a;
+    cout<<j<<endl;
+    return 0;
+}
