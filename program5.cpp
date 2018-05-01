aeb832b2a354571130503019ff7f2a67fe74adbb
Kamal Thakur <kamalthakur2195@gmail.com>
2018-05-01 23:05:33 +0530

    Modified program 5

--- program5.cpp
+++ program5.cpp
@@ -3,10 +3,34 @@
 #include<cstdlib>
 using namespace std;
 int main(){
-	int r_size,c_size, **mat;
+	int r_size,c_size,d_size, ***mat;
 	cout<<"Enter Matric size: ";
-	cin>>r_size>>c_size;
-	**mat = (int *)malloc(sizeof(int)*r_size*c_size);
+	cin>>r_size>>c_size>>d_size;
+	cout<<r_size<<" "<<c_size<<" "<<d_size<<endl;
+	mat = (int ***)malloc(sizeof(int)*r_size);
+	for(int i=0;i<r_size;i++){
+		mat[i]=(int **)malloc(sizeof(int)*c_size);
+		for(int j=0;j<c_size;j++){
+			mat[i][j]=(int *)malloc(sizeof(int)*d_size);
+		}
+	}
+	for(int i=0;i<r_size;i++){
+		for(int j=0;j<c_size;j++){
+			for(int k=0;k<d_size;k++){
+			        cin>>mat[i][j][k];
+			}
+		}
+	}
+	for(int i=0;i<r_size;i++){
+		for(int j=0;j<c_size;j++){
+			for(int k=0;k<d_size;k++){
+			        cout<<mat[i][j][k]<<"  ";
+			}
+			cout<<"\t";
+		}
+		cout<<endl;
+	}
+
 
 
 	return 0;
